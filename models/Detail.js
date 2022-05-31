d8380b531642ff82291a58afc7f84d987cf20006
Piotr Moryto <piotrmoryto@gmail.com>
2022-05-31 20:23:08 +0200

    refactoring

--- models/Detail.js
+++ models/Detail.js
@@ -1,20 +1,18 @@
-const { BasePage } = require('../models/Base');
 const { faker } = require('@faker-js/faker');
 
-exports.DetailPage = class DetailPage extends BasePage {
+exports.DetailPage = class DetailPage {
     constructor(page) {
-        super(page);
-
-        this.nameReviewInput = '#name';
-        this.emailReviewInput = '#email';
-        this.textareaReview = '#review';
-        this.reviewBtn = '#button-review';
+        this.page = page;
+        this.nameReviewInput = page.locator('#name');
+        this.emailReviewInput = page.locator('#email');
+        this.textareaReview = page.locator('#review');
+        this.reviewBtn = page.locator('#button-review');
     }
 
     async addReview() {
-        await this.fill(this.nameReviewInput, faker.name.findName());
-        await this.fill(this.emailReviewInput, faker.internet.email());
-        await this.fill(this.textareaReview, faker.lorem.lines());
-        await this.click(this.reviewBtn);
+        await this.nameReviewInput.fill(faker.name.findName());
+        await this.emailReviewInput.fill(faker.internet.email());
+        await this.textareaReview.fill(faker.lorem.lines());
+        await this.reviewBtn.click();
     }
 }
