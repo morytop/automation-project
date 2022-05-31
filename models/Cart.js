4263efe8735e23d86a754f86089038922d4f6cc6
Piotr Moryto <piotrmoryto@gmail.com>
2022-05-31 16:14:20 +0200

    refactoring

--- models/Cart.js
+++ models/Cart.js
@@ -1,37 +1,38 @@
-const { BasePage } = require('../models/Base');
 const { faker } = require('@faker-js/faker');
 
-exports.CartPage = class CartPage extends BasePage {
+exports.CartPage = class CartPage {
     constructor(page) {
-        super(page);
-
-        this.removeBtn = '.cart_quantity_delete';
-        this.checkoutBtn = 'a[class="btn btn-default check_out"]';
-        this.subscribeInput = '#susbscribe_email';
-        this.subscribeBtn = '#subscribe';
+        this.page = page;
+        this.removeBtn = page.locator('.cart_quantity_delete');
+        this.checkoutBtn = page.locator('a[class="btn btn-default check_out"]');
+        this.subscribeInput = page.locator('#susbscribe_email');
+        this.subscribeBtn = page.locator('#subscribe');
+        this.mainCartBtn = page.locator('.fa.fa-shopping-cart >> nth=0');
+        this.continueShoppingBtn = page.locator('button[class="btn btn-success close-modal btn-block"]');
+        this.addToCartBtn = page.locator('.add-to-cart >> nth=0');
+        this.viewCartBtn = page.locator('p >> a[href="/view_cart"]');
     }
 
     async open() {
-        await this.click('.fa.fa-shopping-cart >> nth=0');
+        await this.mainCartBtn.click();
     }
 
     async addRandomItem() {
         const randomProductIndex = Math.floor(Math.random() * 8) + 1
-        await this.click(`a[data-product-id="${randomProductIndex}"] >> nth=0`);
-        await this.click('//*[contains(text(),"Continue Shopping")]');
+        await this.page.click(`a[data-product-id="${randomProductIndex}"] >> nth=0`);
+        await this.continueShoppingBtn.click();
     }
 
     async addItem() {
-        await this.click('.add-to-cart >> nth=0');
-        await this.page.waitForSelector('p >> a[href="/view_cart"]');
-        await this.click('p >> a[href="/view_cart"]');
+        await this.addToCartBtn.click();
+        await this.viewCartBtn.click();
     }
 
     async remove() {
-        await this.click(this.removeBtn);
+        await this.removeBtn.click();
     }
 
     async proceedToCheckout() {
-        await this.click(this.checkoutBtn);
+        await this.checkoutBtn.click();
     }
 }
